4bcf499b6c23608a01c45352afafe40b6a501e3e
n260450135 <260450135@qq.com>
2020-03-19 08:33:11 +0800

    redisson

--- redis/src/main/java/com/nijunyang/redis/RedisApplication.java
+++ redis/src/main/java/com/nijunyang/redis/RedisApplication.java
@@ -1,13 +1,34 @@
 package com.nijunyang.redis;
 
+import org.redisson.Redisson;
+import org.redisson.config.Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
+	}
+
+	@Bean
+	public Redisson redisson() {
+		Config config = new Config();
+
+		config.useSingleServer().setAddress("redis://111.229.53.45:6379").setPassword("root");
+
+		/**
+		 * 哨兵
+		 */
+		//config.useSentinelServers().addSentinelAddress("");
+
+		/**
+		 * 集群
+		 */
+		//config.useClusterServers().addNodeAddress("redis://111.229.53.45:6379");
+		return (Redisson) Redisson.create(config);
 	}
 
 }
--- redis/src/main/java/com/nijunyang/redis/lock/LockController.java
+++ redis/src/main/java/com/nijunyang/redis/lock/LockController.java
@@ -1,10 +1,15 @@
 package com.nijunyang.redis.lock;
 
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Description:
@@ -19,16 +24,20 @@ public class LockController {
     @Autowired
     RedisTemplate<String, Object> redisTemplate;
 
-    String lock = "lock";
+    @Autowired
+    Redisson redisson;
+
+    String lockKey = "lockKey";
 
     String quantityKey = "quantity";
 
     @GetMapping("/deduct-stock")
     public String deductStock() {
+        String uuid = UUID.randomUUID().toString();
         try {
             //设置值，并且设置超时时间
-//            boolean getLock = valueOperations.setIfAbsent(lock, 1, 10, TimeUnit.SECONDS);
-            boolean getLock = valueOperations.setIfAbsent(lock, 1);
+            boolean getLock = valueOperations.setIfAbsent(lockKey, uuid, 10, TimeUnit.SECONDS);
+//            boolean getLock = valueOperations.setIfAbsent(lock, 1);
             if (!getLock) {
                 return "没有获取到锁";
             }
@@ -43,7 +52,32 @@ public class LockController {
             }
             return "true";
         } finally {
-            redisTemplate.delete(lock);
+            //删除之前判断是否是自己加的锁
+            if (uuid.equals(valueOperations.get(lockKey))) {
+                redisTemplate.delete(lockKey);
+            }
+        }
+    }
+
+
+    @GetMapping("/deduct-stock2")
+    public String deductStock2() {
+        RLock redissonLock = redisson.getLock(lockKey);
+        try {
+            redissonLock.lock();
+            //使用当做数据库，只是模拟扣减库存场景，因此不使用原子操作
+            Integer quantity = (Integer) valueOperations.get(quantityKey);
+            if (quantity > 0) {
+                --quantity;
+                valueOperations.set(quantityKey, quantity);
+                System.out.println("扣减库存成功,剩余库存： " + quantity);
+                return "true";
+            } else {
+                System.out.println("扣减库存成功,剩余库存： " + quantity);
+                return "false";
+            }
+        } finally {
+            redissonLock.unlock();
         }
     }
 
