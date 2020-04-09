11ac8952e162c4fb2b3bfeb04f59d1d4f0fa474a
n260450135 <260450135@qq.com>
2020-04-09 22:48:10 +0800

    1

--- algorithm/src/main/java/com/nijunyang/algorithm/math/Recursion.java
+++ algorithm/src/main/java/com/nijunyang/algorithm/math/Recursion.java
@@ -6,10 +6,34 @@ package com.nijunyang.algorithm.math;
  */
 public class Recursion {
 
+
+
     public static void main(String[] args){
-        int result = recursion(50);
+        System.out.println("---裴波那契数列普通递归---");
+        long time1 = System.currentTimeMillis();
+        int result = recursion(45);
         System.out.println(result);
-        result = cycle(50);
+        long time2 = System.currentTimeMillis();
+        System.out.println(time2-time1);
+        System.out.println("---裴波那契数列尾递归---");
+        result = tailRecursion(1,1,45);
+        System.out.println(result);
+        long time3 = System.currentTimeMillis();
+        System.out.println(time3-time2);
+
+//        result = cycle(30);
+//        System.out.println(result);
+//        result = cache(30);
+//        System.out.println(result);
+
+//        long l = System.currentTimeMillis();
+//        System.out.println(factorial(30));
+//        long l2 = System.currentTimeMillis();
+//        System.out.println(l2-l);
+//        System.out.println(taiFactorial(30, 1));
+//        long l3 = System.currentTimeMillis();
+//        System.out.println(l3-l2);
+
     }
 
     /**
@@ -25,7 +49,7 @@ public class Recursion {
     }
 
     /**
-     * 循环实现
+     * 裴波那契数列循环实现
      * @param n
      * @return
      */
@@ -44,16 +68,60 @@ public class Recursion {
         return fn ;
     }
 
-    public static int tailRecursion(int pre,int res,int n) {
+    /**
+     * 尾递归 裴波那契
+     * @param pre 上上一次运算出来的结果
+     * @param result  上一次运算出来结果
+     * @param n
+     * @return
+     */
+    public static int tailRecursion(int pre, int result, int n) {
+        if (n <= 2) {
+            return result;
+        }
+        //对于下一次调用来说 前一次结果 pre + result  前前一次result
+        return tailRecursion(result, pre + result, n - 1);
+    }
+
+    public static int cache(int n) {
+        int data[] = new int[n]; // 用数组来做缓存
+        return fac(n, data);
+    }
+
+    public static int fac(int n, int[] data) {
         if (n <= 2)
-            return res; // 递归的终止条件
-        return tailRecursion(res, pre + res, n - 1);
-        //参数：
-        /**
-         * n 是肯定有的
-         * res 上一次运算出来结果
-         * pre 上上一次运算出来的结果
-         */
+            return 1; //递归的终止条件
+        if (data[n-1] > 0) {  //数组中有值就直接取出来返回，不用再去计算
+            return data[n-1];
+        }
+        int res = fac(n - 1, data) + fac(n - 2, data);
+        data[n-1] = res;  //算出来值放到数组中
+        return res;
+    }
+
+
+    /**
+     * 阶乘
+     * @param n
+     * @return
+     */
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    /**
+     * 阶乘尾递归
+     * @param n
+     * @return
+     */
+    public static int taiFactorial(int n, int result) {
+        if (n <= 1) {
+            return result; //最后返回的即是最终结果
+        }
+        return taiFactorial(n - 1, n * result);//结果往下传
     }
 
 
