68c3838365273264057a18600b25082e18047b7c
n260450135 <260450135@qq.com>
2020-04-19 23:50:38 +0800

    查询

--- algorithm/src/main/java/com/nijunyang/algorithm/tree/BinarySearchTree.java
+++ algorithm/src/main/java/com/nijunyang/algorithm/tree/BinarySearchTree.java
@@ -4,7 +4,7 @@ package com.nijunyang.algorithm.tree;
  * Description:
  * Created by nijunyang on 2020/4/19 20:03
  */
-public class BinarySearchTree<T extends Comparable> {
+public class BinarySearchTree<T extends Comparable<T>> {
     private T data;
     private BinarySearchTree<T> leftChild;
     private BinarySearchTree<T> rightChild;
@@ -13,7 +13,7 @@ public class BinarySearchTree<T extends Comparable> {
         this.data = data;
     }
 
-    public static<T extends Comparable> void insert(BinarySearchTree root, T data) {
+    public static<T extends Comparable<T>> void insert(BinarySearchTree<T> root, T data) {
         if (root.data.compareTo(data) < 0) {
             if (root.rightChild == null) {
                 root.rightChild = new BinarySearchTree(data);
@@ -30,6 +30,26 @@ public class BinarySearchTree<T extends Comparable> {
             }
         }
     }
+
+
+    public static<T extends Comparable<T>> BinarySearchTree<T> find(BinarySearchTree<T> root, T data) {
+        if (root.data.compareTo(data) == 0) {
+            return root;
+        }
+        if (root.data.compareTo(data) < 0) {
+            if (root.rightChild != null) {
+                find(root.rightChild, data);
+            }
+        }
+        else {
+            if (root.leftChild != null) {
+                find(root.leftChild, data);
+            }
+        }
+        return null;
+    }
+
+
 
 
 
