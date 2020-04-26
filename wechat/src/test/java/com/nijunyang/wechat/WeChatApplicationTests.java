fc1daf06e82bf527fba44b84454aad8e73a66ba9
nijunyang <junyang.ni@sao.so>
2020-04-26 18:32:35 +0800

    微信demo后台

---
+++ wechat/src/test/java/com/nijunyang/wechat/WeChatApplicationTests.java
@@ -0,0 +1,14 @@
+package com.nijunyang.wechat;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
